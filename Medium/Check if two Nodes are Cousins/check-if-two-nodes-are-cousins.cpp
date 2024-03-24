67f31540c7d794a5db401956a1d25d504ddcd95c
Deep Isane <146447579+Deep145757@users.noreply.github.com>
2024-03-24 23:56:46 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Check if two Nodes are Cousins/check-if-two-nodes-are-cousins.cpp
@@ -0,0 +1,162 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+// Function to Build Tree
+Node* buildTree(string str)
+{   
+    // Corner Case
+    if(str.length() == 0 || str[0] == 'N')
+            return NULL;
+    
+    // Creating vector of strings from input 
+    // string after spliting by space
+    vector<string> ip;
+    
+    istringstream iss(str);
+    for(string str; iss >> str; )
+        ip.push_back(str);
+        
+    // Create the root of the tree
+    Node* root = new Node(stoi(ip[0]));
+        
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+        
+    // Starting from the second element
+    int i = 1;
+    while(!queue.empty() && i < ip.size()) {
+            
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+            
+        // Get the current node's value from the string
+        string currVal = ip[i];
+            
+        // If the left child is not null
+        if(currVal != "N") {
+                
+            // Create the left child for the current node
+            currNode->left = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+            
+        // For the right child
+        i++;
+        if(i >= ip.size())
+            break;
+        currVal = ip[i];
+            
+        // If the right child is not null
+        if(currVal != "N") {
+                
+            // Create the right child for the current node
+            currNode->right = new Node(stoi(currVal));
+                
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+    
+    return root;
+}
+
+bool isCousins(Node*root , int x , int y);
+
+int main()
+{
+
+    int t;
+	scanf("%d ",&t);
+    while(t--)
+    {
+        string s;
+		getline(cin,s);
+        Node* root = buildTree(s);
+        int x , y;
+        scanf("%d ",&x);
+		scanf("%d ",&y);
+        if(x==y)
+        {
+            cout<<"0\n";
+            continue;
+        }
+        cout <<isCousins(root,x,y)<< endl;
+    }
+    return 1;
+}
+// } Driver Code Ends
+
+
+/*Complete the function below
+Node is as follows:
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+// Returns true if the nodes with values 'a' and 'b' are cousins. Else returns false
+Node *p2;
+Node *p1;
+int h1, h2;
+//bool f1, f2;
+
+void traverse(Node *root, int a, int b, int h){
+    h++;
+    if(root->left){
+        if(root->left->data == a){
+            p1 = root;
+            h1 = h;
+        }
+        if(root->left->data == b){
+            p2 = root;
+            h2 = h;
+        }
+        traverse(root->left, a, b, h);
+    }
+    
+    if(root->right){
+        if(root->right->data == a){
+            p1 = root;
+            h1 = h;
+        }
+        if(root->right->data == b){
+            p2 = root;
+            h2 = h;
+        }
+        traverse(root->right, a, b, h);
+    }
+}
+
+
+bool isCousins(Node *root, int a, int b)
+{
+   //add code here.
+    h1 = -1, h2 = -2;
+    if(root)
+       traverse(root, a, b, 0);
+    if(h1 == h2 && p1 != p2)
+        return 1;
+    return 0;
+}
