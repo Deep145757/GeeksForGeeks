6cc12e1ad9749cac8640cec388d545bfa73b1644
Deep Isane <146447579+Deep145757@users.noreply.github.com>
2024-07-08 21:25:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int search(vector<int>& arr, int key) {
+        // complete the function here
+        bool issmall=0;
+        int n = arr.size();
+        for(int i=0; i<n; i++){
+            if(key == arr[i])
+                return i;
+            else if(key  > arr[i])
+                issmall = 1;
+            else if(key > arr[i]){
+                if(issmall)
+                    return -1;
+            }
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        cin.ignore();
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        int key;
+        cin >> key;
+        Solution ob;
+        cout << ob.search(arr, key) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
