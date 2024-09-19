4d0a2660c5227cd3a0cf457a74e25d16ec05855c
Deep Isane <146447579+Deep145757@users.noreply.github.com>
2024-09-19 08:41:30 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Reverse Words/reverse-words.cpp
+++ Difficulty: Easy/Reverse Words/reverse-words.cpp
@@ -2,47 +2,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 
-class Solution
-{
-    public:
-    //Function to reverse words in a given string.
-    string reverseWords(string S) 
-    { 
-        // code here 
-        string ans="", temp="";
-        for(int i=0; i<S.size(); i++){
-            if(S[i] != '.'){
-                temp = temp + S[i];
+class Solution {
+  public:
+    // Function to reverse words in a given string.
+    string reverseWords(string str) {
+        // code here
+        vector<string>arr;
+        string temp = "";
+        int n = str.size();
+        for(int i=0; i<n; i++){
+            if(str[i] != '.'){
+                temp = temp + str[i];
             }
             else{
-                if(ans != ""){
-                    ans = '.' + ans;
-                }
-                ans = temp + ans;
+                arr.push_back(temp);
                 temp = "";
             }
         }
-        if(ans != ""){
-           ans = '.' + ans;
+        arr.push_back(temp);
+        string ans = "";
+        for(int i=arr.size()-1; i>=0; i--){
+            ans = ans + arr[i];
+            if(i != 0)
+                ans = ans + '.';
         }
-        ans = temp + ans;
         return ans;
-    } 
+    }
 };
 
 //{ Driver Code Starts.
-int main() 
-{
+int main() {
     int t;
     cin >> t;
-    while (t--) 
-    {
+    while (t--) {
         string s;
         cin >> s;
         Solution obj;
-        cout<<obj.reverseWords(s)<<endl;
+        cout << obj.reverseWords(s) << endl;
     }
 }
 // } Driver Code Ends
