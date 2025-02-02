e65c5fa075521bab81e13f8fdf3fa5dcee3871f0
Deep Isane <146447579+Deep145757@users.noreply.github.com>
2025-02-02 10:36:42 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.cpp
@@ -0,0 +1,117 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* Link list Node */
+struct Node {
+    int data;
+    struct Node* next;
+
+    Node(int x) {
+        data = x;
+        next = nullptr;
+    }
+};
+
+void printList(Node* node) {
+    while (node != NULL) {
+        cout << node->data << " ";
+        node = node->next;
+    }
+    cout << "\n";
+}
+
+void freeList(struct Node* head) {
+    struct Node* temp;
+    while (head != NULL) {
+        temp = head;
+        head = head->next;
+        delete temp;
+    }
+}
+
+
+// } Driver Code Ends
+/*
+
+  Node is defined as
+  struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+class Solution {
+  public:
+    // Function to sort a linked list of 0s, 1s and 2s.
+    Node* segregate(Node* head) {
+
+        // Add code here
+        vector<int>counts(3, 0);
+        Node *ptr = head;
+        while(ptr){
+            counts[ptr->data]++;
+            ptr = ptr->next;
+        }
+        
+        ptr = head;
+        for(int i=0; i<counts[0]; i++){
+            ptr->data = 0;
+            ptr = ptr->next;
+        }
+        for(int i=0; i<counts[1]; i++){
+            ptr->data = 1;
+            ptr = ptr->next;
+        }
+        for(int i=0; i<counts[2]; i++){
+            ptr->data = 2;
+            ptr = ptr->next;
+        }
+        return head;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+// Driver program to test above functions
+int main() {
+    int t, k;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after t
+
+    while (t--) {
+        string input;
+        getline(cin, input); // Read the entire line for the array elements
+
+        stringstream ss(input);
+        Node *head = nullptr, *tail = nullptr;
+        int x;
+
+        if (ss >> x) {
+            head = new Node(x);
+            tail = head;
+
+            while (ss >> x) {
+                tail->next = new Node(x);
+                tail = tail->next;
+            }
+
+            // Link the last node to the head if k is 1 to make it circular
+            if (k == 1) {
+                tail->next = head;
+            }
+        }
+
+        Solution ob;
+        Node* newHead = ob.segregate(head);
+        printList(newHead);
+    }
+
+    return 0;
+}
+// } Driver Code Ends
