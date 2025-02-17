7470d6bcf76ff502475737bbf3d9e2a42cb4f14a
Deep Isane <146447579+Deep145757@users.noreply.github.com>
2025-02-17 22:41:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Fill the Matrix/fill-the-matrix.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{   
+public:
+    int minIteration(int N, int M, int x, int y){    
+        // code here
+        int hr = max(N-x, x-1);
+        int vr = max(M-y, y-1);
+        
+        return hr+vr;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, M, x, y;
+        cin >> N >> M;
+        cin >> x >> y;
+        
+        Solution ob;
+        cout << ob.minIteration(N, M, x, y) << endl;
+    
+cout << "~" << "\n";
+}
+    return 0; 
+} 
+// } Driver Code Ends
