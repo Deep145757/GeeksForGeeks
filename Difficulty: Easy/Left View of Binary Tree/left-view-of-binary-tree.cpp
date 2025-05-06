3694906f5f45060c4f9f8caf669d9bc3752ae3da
Deep Isane <146447579+Deep145757@users.noreply.github.com>
2025-05-06 22:08:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
+++ Difficulty: Easy/Left View of Binary Tree/left-view-of-binary-tree.cpp
@@ -3,18 +3,54 @@
 using namespace std;
 
 // Tree Node
-struct Node
-{
+struct Node {
     int data;
     Node* left;
     Node* right;
 };
 
-vector<int> leftView(struct Node *root);
+
+// } Driver Code Ends
+
+/* A binary tree node
+
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};
+ */
+
+class Solution {
+  public:
+    void travel(Node *root, vector<int>&ans, int h){
+        if(!root)
+            return;
+        if(h >= ans.size())
+            ans.push_back(root->data);
+        travel(root->left, ans, h+1);
+        travel(root->right, ans, h+1);
+    }
+  
+    vector<int> leftView(Node *root) {
+        // code here
+        vector<int>ans;
+        travel(root, ans, 0);
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
 
 // Utility function to create a new Tree Node
-Node* newNode(int val)
-{
+Node* newNode(int val) {
     Node* temp = new Node;
     temp->data = val;
     temp->left = NULL;
@@ -23,12 +59,10 @@ Node* newNode(int val)
     return temp;
 }
 
-
 // Function to Build Tree
-Node* buildTree(string str)
-{
+Node* buildTree(string str) {
     // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
+    if (str.length() == 0 || str[0] == 'N')
         return NULL;
 
     // Creating vector of strings from input
@@ -36,7 +70,7 @@ Node* buildTree(string str)
     vector<string> ip;
 
     istringstream iss(str);
-    for(string str; iss >> str; )
+    for (string str; iss >> str;)
         ip.push_back(str);
 
     // for(string i:ip)
@@ -51,7 +85,7 @@ Node* buildTree(string str)
 
     // Starting from the second element
     int i = 1;
-    while(!queue.empty() && i < ip.size()) {
+    while (!queue.empty() && i < ip.size()) {
 
         // Get and remove the front of the queue
         Node* currNode = queue.front();
@@ -61,7 +95,7 @@ Node* buildTree(string str)
         string currVal = ip[i];
 
         // If the left child is not null
-        if(currVal != "N") {
+        if (currVal != "N") {
 
             // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
@@ -72,12 +106,12 @@ Node* buildTree(string str)
 
         // For the right child
         i++;
-        if(i >= ip.size())
+        if (i >= ip.size())
             break;
         currVal = ip[i];
 
         // If the right child is not null
-        if(currVal != "N") {
+        if (currVal != "N") {
 
             // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
@@ -91,60 +125,26 @@ Node* buildTree(string str)
     return root;
 }
 
-
 int main() {
     int t;
-    scanf("%d ",&t);
-    while(t--)
-    {
+    scanf("%d ", &t);
+    while (t--) {
         string s;
-        getline(cin,s);
+        getline(cin, s);
         Node* root = buildTree(s);
-        vector<int> vec = leftView(root);
-        for(int x : vec)
-        cout<<x<<" ";
+        Solution ob;
+        vector<int> vec = ob.leftView(root);
+        if (vec.size() == 0) {
+            cout << "[]";
+        }
+        for (int x : vec)
+            cout << x << " ";
         cout << endl;
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
 
-
 // } Driver Code Ends
-
-
-/* A binary tree node
-
-struct Node
-{
-    int data;
-    struct Node* left;
-    struct Node* right;
-    
-    Node(int x){
-        data = x;
-        left = right = NULL;
-    }
-};
- */
-
-//Function to return a list containing elements of left view of the binary tree.
-void trav(Node *root, int n, vector<int>&lv){
-    if(!root)
-        return;
-        
-    if(n == lv.size())
-        lv.push_back(root->data);
-        
-    trav(root->left, n+1, lv);
-    trav(root->right, n+1, lv);
-    
-}
-
-
-vector<int> leftView(Node *root)
-{
-   // Your code here
-   vector<int> lv;
-   trav(root, 0, lv);
-   return lv;
-}
